d823b4a7c9393b13c6b705936d694473736f08dc
diegobsilva10 <diegobsilva12@gmail.com>
2021-08-09 12:24:48 -0300

    SUPER CLASSE PESSOA

---
+++ POO/src/exercicios/TesteEmpregado3.java
@@ -0,0 +1,11 @@
+package exercicios;
+
+
+public class TesteEmpregado3 {
+    public static void main (String[] args){
+        Empregado1 diego = new Empregado1("Diego","Rua moro ali","1234567891011",
+                40028922, 24, 29, 5000,12);
+        diego.imprimirInfo();
+        diego.calcularSalario();
+    }
+}
