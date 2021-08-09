81f68db8b5eed6c02fbbeeedfc29abae0fe008a3
diegobsilva10 <diegobsilva12@gmail.com>
2021-08-09 12:11:36 -0300

    SUPER CLASSE PESSOA

---
+++ POO/src/exercicios/Operario.java
@@ -0,0 +1,42 @@
+package exercicios;
+
+public class Operario extends Pessoa {
+    private double valorProducao;
+    private double comissao;
+
+    public Operario(String nome, String endereco, String cpf,
+                    int telefone, int idade, double valorProducao, double comissao) {
+        super(nome, endereco, cpf, telefone, idade);
+        this.valorProducao = valorProducao;
+        this.comissao = comissao;
+    }
+
+    public void imprimirInfo() {
+        System.out.println("Nome do operario: " + getNome() + "\nCPF: " + getCpf() + "\nIdade: "
+                + getIdade() + "\nTelefone: " + getTelefone() + "\nEndereço: " + getEndereco()
+                + "\nValor monetário total dos artigos produzidos por " + getNome()
+                + " : " + valorProducao + "\nPorcentagem (sem o %) da comissão deste artigo" + comissao);
+
+    }
+
+    public void calcularValorArtigo() {
+        double valor_total = valorProducao + (valorProducao * (comissao / 100));
+        System.out.println("\nO valor total a ser recebido pelo operario: " + getNome() + " é igual a: " + valor_total);
+    }
+
+    public double getValorProducao() {
+        return valorProducao;
+    }
+
+    public void setValorProducao(double valorProducao) {
+        this.valorProducao = valorProducao;
+    }
+
+    public double getComissao() {
+        return comissao;
+    }
+
+    public void setComissao(double comissao) {
+        this.comissao = comissao;
+    }
+}
