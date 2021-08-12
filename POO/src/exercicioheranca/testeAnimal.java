2e33b20a96e507f79fd9c34f553f7c3dab5ec988
diegobsilva10 <diegobsilva12@gmail.com>
2021-08-12 15:28:48 -0300

    commit

--- POO/src/exercicioheranca/testeAnimal.java
+++ POO/src/exercicioheranca/testeAnimal.java
@@ -2,7 +2,7 @@ package exercicioheranca;
 
 public class testeAnimal {
     public static void main (String [] args){
-        Animal cavalinho = new Cavalo("cavalo", 7, "baru", "sim", "nao");
+        Animal cavalinho = new Cavalo("cavaloo", 7, "baru", "sim", "nao");
         Preguica pre = new Preguica ("pre",7,"barulho","s", "n");
         System.out.println(cavalinho.getNome());
         System.out.println(cavalinho.getIdade());
